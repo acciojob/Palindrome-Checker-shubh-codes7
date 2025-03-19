e06e2684b2ccf50f22fadab4c7b1bfcb52752fd9
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-19 10:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.replaceAll(' ','').toLowerCase()
+	let start = 0;
+	let end = str.length-1;
+	
+	while(start < end){
+	    if(str[start++] !== str[end--]) return false
+	}
+	return true;
 }
 module.exports = palindrome
